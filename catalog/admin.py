dd68cc32211e0c27d48d7d7873f918c1cfa5080a
origin <yura28049119@yandex.ru>
2025-06-08 17:57:35 +0300

    feature/homework23

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", 'id', "purchase_price", 'category')
+    list_filter = ('category',)
+    search_fields = ("name", "description")
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", 'id')
